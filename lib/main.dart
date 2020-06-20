40f915b7ee6689dd6259915f325215391d2492cb
lakshaymadhav <lakshaymadhav25@gmail.com>
2020-06-20 08:37:59 +0530

    Updated some elements

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    list.add("nr.1");
-    list.add("nr.2");
-    list.add("nr.3");
-    list.add("nr.4");
+    list.add("Welcome to To-Do List");
+    list.add("Use the + Button to Add an Entry to the List ");
+    list.add("Swipe Right to delete an Entry in the list");
+    list.add("New Features coming on!!");
+    list.add("To Request a feature, Ping me on ");
+    list.add("Email: lakshaymadhav25@gmail.com");
   }
 
   @override
@@ -49,22 +51,23 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               context: context,
               builder: (BuildContext context){
                 return AlertDialog(
-                  title: Text("Add ToDo"),
+                  title: Text("Add New List"),
                   content: TextField(
                     onChanged: (String value){
                       input = value; 
                     },
                   ),
                   actions: <Widget>[
-                    FlatButton(
-                      child: Text("add") ,
+                    RaisedButton(
+                      child: Text("Add"),
+                      color: Colors.blue[300],
                       onPressed: (){
                         setState(() {
-                         list.add(input); 
+                          list.add(input); 
                         });
                         Navigator.of(context).pop();
                       },
-                    )
+                    ),
                   ],
                 );
               },
@@ -72,21 +75,27 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           },
         ), 
         body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (BuildContext context,int index) {
-            return Dismissible(
-              key: Key(list[index]), 
-              child: ListTile(
-                title: Text(list[index])
-              ),
-              onDismissed: (direction){
-                setState(() {
-                  list.removeAt(index);
-                });
-              },
+            itemCount: list.length,
+            itemBuilder: (BuildContext context,int index) {
+              return Padding(
+                padding: EdgeInsets.only(top:5.0,left: 5.0,right: 5.0),
+                child: Card(
+                        child: Dismissible(
+                    key: Key(list[index]),
+                    
+                    child:ListTile(
+                      title: Text(list[index])
+                    ),
+                    onDismissed: (direction){
+                      setState(() {
+                        list.removeAt(index);
+                      });
+                    },   
+                  ),
+                 ),
               );
-          } ,
-        ),
+            } ,
+          ), 
     );
      
   }
